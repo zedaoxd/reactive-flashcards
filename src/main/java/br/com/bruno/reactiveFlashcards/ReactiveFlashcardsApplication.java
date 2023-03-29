24f1be8c7a770962179cb3d013c7b7d93ed874b7
Bruno Lessa <brunolessa@live.com>
2023-03-29 14:12:24 -0300

    create project

---
+++ src/main/java/br/com/bruno/reactiveFlashcards/ReactiveFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno.reactiveFlashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFlashcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveFlashcardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bruno/reactiveFlashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bruno.reactiveFlashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
